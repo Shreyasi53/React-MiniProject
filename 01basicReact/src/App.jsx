f589ab56ea704f9522592f60f907d48475a59534
Shreyasi53 <shreyasidutta53@gmail.com>
2025-12-10 19:22:53 +0530

    feat: implement custom render function and update index.html

--- 01basicReact/src/App.jsx
+++ 01basicReact/src/App.jsx
@@ -1,8 +1,11 @@
 import Test from "./test";
 function App() {
+  const username = "Chai aur code"
+//{username} is called evaluated expression 
   return (
     <>
       <Test/>
+      <h1>chai aur react {username}</h1>  
     </>
     
   )
