ae0b767a06c441d3b2d1db2a12e34128df8e4450
Shreyasi53 <shreyasidutta53@gmail.com>
2025-12-27 20:09:20 +0530

    ClassBased news app

---
+++ NewsApp/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    const name = "shreyasi"
+    return (
+      <div>
+        <h2>this is a class base component </h2>{name}
+      </div>
+    )
+  }
+}
